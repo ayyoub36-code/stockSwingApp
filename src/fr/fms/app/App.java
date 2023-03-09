9f495fb5de8bbc7d2beb98affc2e0649fd5a3e38
Ayyoub MEHDIOUI <MehdiouiA@ad.fms-ea.com>
2023-03-09 10:23:00 +0100

    todo

--- src/fr/fms/app/App.java
+++ src/fr/fms/app/App.java
@@ -3,7 +3,11 @@ package fr.fms.app;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO script sql creation de la table produit et bd = stock => benoit
+		// TODO entitie product => guillaume
+		// TODO Dao (interface) et son implementation readAll => charger tous les
+		// produits => aurore
+		// TODO bdd connection => singleton => françois
 
 	}
 
---
+++ src/fr/fms/bdd/BddConnection.java
@@ -0,0 +1,5 @@
+package fr.fms.bdd;
+
+public class BddConnection {
+
+}
