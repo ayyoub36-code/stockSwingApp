35ab476eca7a6910186278d263ddd2f15d32717e
Ayyoub MEHDIOUI <MehdiouiA@ad.fms-ea.com>
2023-03-09 11:42:21 +0100

    add class todo

--- src/fr/fms/app/App.java
+++ src/fr/fms/app/App.java
@@ -3,9 +3,9 @@ package fr.fms.app;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO view JButton + JTable(model)
-		// TODO controller actionListner => event (view)
-		// TODO model data bd => dao for remplir le tableau
+		// TODO view JButton + JTable(model) ayyoub
+		// TODO controller actionListner => event (view) françois + benoit
+		// TODO model data bd => dao for remplir le tableau aurore + guillaume
 
 //		Dao<Product> dao = new ProductDao();
 //		dao.readAll().forEach(e -> System.out.println(e));
